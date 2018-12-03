c9dc9bac7a7699e1c00e06cf3a7b558537e8beaa
corey <corey.craddock@gmail.com>
2018-12-03 11:48:53 -0700

    Added support for recurring calendar events to show up on the calendar panel

--- scripts/index.js
+++ scripts/index.js
@@ -16,7 +16,7 @@ const DATETIME = (function () {
         d = new Date();
         let timeString = '';
         let end = '';
-        if (d.getHours() >= 12) {
+        if (d.getHours() > 12) {
             timeString += d.getHours() % 12;
             end = 'PM';
         } else {
@@ -56,7 +56,7 @@ const WEATHER = (function () {
 
         axios.get(geocodeURL)
             .then((res) => {
-                if (res.data.results.length == 0) {
+                if (res.data.results.length === 0) {
                     console.error("Invalid location");
                     return;
                 }
@@ -137,10 +137,10 @@ const CALENDAR = (function () {
         drawableEvents = [];
 
     const _getFile = () => {
-        while(drawableEvents.length > 0) {
+        while (drawableEvents.length > 0) {
             drawableEvents.pop();
         }
-        while(events.length > 0) {
+        while (events.length > 0) {
             events.pop();
         }
         let rawFile = new XMLHttpRequest();
@@ -161,20 +161,42 @@ const CALENDAR = (function () {
             if (!r.includes("BEGIN:VEVENT")) {
                 return;
             }
-            let name = rows[index + 1].substring(8),
-                start = rows[index + 3],
-                startTime = '',
-                end = rows[index + 4],
+            let name = '',
+                start = '',
+                startTime = 'T00:00:00',
+                end = '',
                 endTime = '',
-                loc = rows[index + 5].substring(9);
+                loc = '',
+                recur = '';
+            for (let i = index + 1; ; i++) {
+                let atI = rows[i];
+
+                if (atI.includes("END:VEVENT"))
+                    break;
+
+                if (atI.includes("SUMMARY:"))
+                    name = atI.substring(8);
+                else if (atI.includes("DTSTART"))
+                    start = atI;
+                else if (atI.includes("DTEND"))
+                    end = atI;
+                else if (atI.includes("LOCATION:"))
+                    loc = atI.substring(9);
+                else if (atI.includes("RRULE:"))
+                    recur = atI.substring(6);
+            }
             for (let i = 0; i < start.length; i++) {
                 if (!isNaN(start.charAt(i))) {
                     start = start.substring(i);
                     break;
                 }
             }
+            if (name.includes("TBA"))
+                console.log("FOUND");
             if (start.length > 8) {
-                startTime = start.substring(8, 11) + ":" + start.substring(11, 13) + ":" + start.substring(13, 15) + "Z";
+                if (start.charAt(start.length - 1) == 'Z')
+                    startTime = start.substring(8, 11) + ":" + start.substring(11, 13) + ":" + start.substring(13, 15) + "Z";
+                else startTime = start.substring(8, 11) + ":" + start.substring(11, 13) + ":" + start.substring(13, 15);
             }
             start = start.substring(0, 4) + "-" + start.substring(4, 6) + "-" + start.substring(6, 8);
             for (let i = 0; i < end.length; i++) {
@@ -191,9 +213,10 @@ const CALENDAR = (function () {
                 name: name,
                 start: new Date(start + startTime),
                 end: new Date(end + endTime),
-                loc: loc
+                loc: loc,
+                recur: recur
             };
-            if (event.end >= date)
+            if (event.end >= date || recur != '')
                 events.push(event);
         });
     };
@@ -209,7 +232,8 @@ const CALENDAR = (function () {
 
         events.sort(eventsCompare);
         for (let i = 0; i < events.length; i++) {
-            let len = events[i].end.getDate() - events[i].start.getDate(),
+            // let len = events[i].end.getDate() - events[i].start.getDate(),
+            let len = Math.round(Math.abs((events[i].start.getTime() - events[i].end.getTime()) / (24 * 60 * 60 * 1000))) - 1,
                 d = events[i].start;
             if (len > 0)
                 d = new Date(d.toDateString());
@@ -220,7 +244,7 @@ const CALENDAR = (function () {
             };
             drawableEvents.push(e);
             for (let j = 1; j <= len; j++) {
-                let d = events[i].start.getTime() + (j * (60 * 60 * 24 * 1000))
+                let d = events[i].start.getTime() + (j * (60 * 60 * 24 * 1000));
                 e = {
                     date: new Date(new Date(d).toDateString()),
                     loc: events[i].loc,
@@ -228,12 +252,249 @@ const CALENDAR = (function () {
                 };
                 drawableEvents.push(e);
             }
+            if (events[i].recur != '') {
+                recurringEvents(e, events[i].recur);
+            }
+        }
+
+        function recurringEvents(e, recur) {
+            recur = recur.split(";");
+            let freq = '',
+                interval = '',
+                count = '',
+                until = '',
+                byDay = '',
+                byMonthDay = '',
+                bySetPos = '',
+                byMonth = '';
+
+            for (let i = 0; i < recur.length; i++) {
+                if (recur[i].includes("FREQ"))
+                    freq = recur[i].split("=")[1];
+                else if (recur[i].includes("INTERVAL"))
+                    interval = recur[i].split("=")[1];
+                else if (recur[i].includes("COUNT"))
+                    count = recur[i].split("=")[1];
+                else if (recur[i].includes("UNTIL"))
+                    until = recur[i].split("=")[1];
+                else if (recur[i].includes("BYDAY"))
+                    byDay = recur[i].split("=")[1];
+                else if (recur[i].includes("BYMONTHDAY"))
+                    byMonthDay = recur[i].split("=")[1];
+                else if (recur[i].includes("BYSETPOS"))
+                    bySetPos = recur[i].split("=")[1];
+                else if (recur[i].includes("BYMONTH"))
+                    byMonth = recur[i].split("=")[1];
+            }
+
+            switch (freq) {
+                case "HOURLY":
+                    // recEvHourly(e, interval, until, count);
+                    break;
+                case "DAILY":
+                    recEvDaily(e, byDay, interval, until, count);
+                    break;
+                case "WEEKLY":
+                    // recEvWeekly(e, byDay, interval, until, count);
+                    break;
+                case "MONTHLY":
+                    // recEvMonthly(e, byMonthDay, bySetPos, byDay, interval, until, count);
+                    break;
+                case "YEARLY":
+                    // recEvYearly(e, byDay, bySetPos, byMonth, byMonthDay, until, count);
+                    break;
+                default:
+                    console.log("\n\n===============\tERROR: " + freq + "\t===============\n\n");
+                    break;
+            }
+
+        }
+
+        function recEvHourly(e, interval, until, count) {
+            console.log("\nHOURLY");
+            console.log("NAME:\t\t\t" + e.name);
+            console.log("LOCATION:\t\t" + e.loc);
+            console.log("DATE:\t\t\t" + e.date);
+            console.log("INTERVAL:\t\t" + interval);
+            console.log("UNTIL:\t\t\t" + until);
+            console.log("COUNT:\t\t\t" + count);
+        }
+
+        function recEvDaily(e, byDay, interval, until, count) {
+            console.log("\nDAILY");
+            console.log("NAME:\t\t\t" + e.name);
+            console.log("LOCATION:\t\t" + e.loc);
+            console.log("DATE:\t\t\t" + e.date);
+            console.log("BYDAY:\t\t\t" + byDay);
+            console.log("INTERVAL:\t\t" + interval);
+            console.log("UNTIL:\t\t\t" + until);
+            console.log("COUNT:\t\t\t" + count);
+
+            // Recur: By day
+            if (byDay != '') {
+                byDay = byDay.split(",");
+                let days = ["SU", "MO", "TU", "WE", "TH", "FR", "SA"];
+                for (let i = 0; i < byDay.length; i++)
+                    byDay[i] = days.indexOf(byDay[i]);
+
+                // End: after
+                if (count != '') {
+                    for (let i = 1; i < count; i++) {
+                        let dayNum = e.date.getDay() + i;
+                        let newE = {
+                            date: new Date(e.date),
+                            loc: e.loc,
+                            name: e.name
+                        };
+                        while(byDay.indexOf(dayNum % 7) == -1) {
+                            dayNum++;
+                        }
+                        newE.date.setDate(newE.date.getDate() + Math.abs(newE.date.getDay() - dayNum));
+                        if ((newE.date.getTime() - new Date().getTime()) / (24 * 60 * 60 * 1000) > 365)
+                            break;
+                        drawableEvents.push(newE);
+                    }
+                }
+                // End: on date
+                else if (until != '') {
+                    until = until.substring(0, 4) + '-' + until.substring(4, 6) + '-' + until.substring(6, 8);
+                    let d = new Date(until + "T00:00:00"),
+                        len = Math.round(Math.abs((e.date.getTime() - d.getTime()) / (24 * 60 * 60 * 1000)));
+                    for (let i = 1; i <= len; i++) {
+                        let dayNum = e.date.getDay() + i;
+                        let newE = {
+                            date: new Date(e.date),
+                            loc: e.loc,
+                            name: e.name
+                        };
+                        while(byDay.indexOf(dayNum % 7) == -1) {
+                            dayNum++;
+                            len--;
+                        }
+                        newE.date.setDate(newE.date.getDate() + Math.abs(newE.date.getDay() - dayNum));
+                        if ((newE.date.getTime() - new Date().getTime()) / (24 * 60 * 60 * 1000) > 365)
+                            break;
+                        drawableEvents.push(newE);
+                    }
+                }
+                // End: Never
+                else
+                {
+                    for (let i = 1; ; i++) {
+                        let dayNum = e.date.getDay() + i;
+                        let newE = {
+                            date: new Date(e.date),
+                            loc: e.loc,
+                            name: e.name
+                        };
+                        while(byDay.indexOf(dayNum % 7) == -1) {
+                            dayNum++;
+                            i++;
+                        }
+                        newE.date.setDate(newE.date.getDate() + Math.abs(newE.date.getDay() - dayNum));
+                        if ((newE.date.getTime() - new Date().getTime()) / (24 * 60 * 60 * 1000) > 365)
+                            break;
+                        drawableEvents.push(newE);
+                    }
+                }
+            }
+            // Recur: By interval
+            else {
+                // End: after
+                if (count != '') {
+                    for (let i = 1; i < count; i++) {
+                        let newE = {
+                            date: new Date(e.date),
+                            loc: e.loc,
+                            name: e.name
+                        };
+                        newE.date.setDate(newE.date.getDate() + (i * interval));
+                        if ((newE.date.getTime() - new Date().getTime()) / (24 * 60 * 60 * 1000) > 365)
+                            break;
+                        drawableEvents.push(newE);
+                    }
+                }
+                // End: on date
+                else if (until != '') {
+                    until = until.substring(0, 4) + '-' + until.substring(4, 6) + '-' + until.substring(6, 8);
+                    let d = new Date(until + "T00:00:00"),
+                        len = Math.round(Math.abs((e.date.getTime() - d.getTime()) / (24 * 60 * 60 * 1000)));
+                    for (let i = 1; i <= len; i++) {
+                        let newE = {
+                            date: new Date(e.date),
+                            loc: e.loc,
+                            name: e.name
+                        };
+                        newE.date.setDate(newE.date.getDate() + (i * interval));
+                        if ((newE.date.getTime() - new Date().getTime()) / (24 * 60 * 60 * 1000) > 365)
+                            break;
+                        drawableEvents.push(newE);
+                    }
+                }
+                // End: Never
+                else {
+                    for (let i = 1; ; i++) {
+                        let newE = {
+                            date: new Date(e.date),
+                            loc: e.loc,
+                            name: e.name
+                        };
+                        newE.date.setDate(newE.date.getDate() + (i * interval));
+                        if ((newE.date.getTime() - new Date().getTime()) / (24 * 60 * 60 * 1000) > 365)
+                            break;
+                        drawableEvents.push(newE);
+                    }
+                }
+            }
+
+        }
+
+        function recEvWeekly(e, byDay, interval, until, count) {
+            console.log("\nWEEKLY");
+            console.log("NAME:\t\t\t" + e.name);
+            console.log("LOCATION:\t\t" + e.loc);
+            console.log("DATE:\t\t\t" + e.date);
+            console.log("INTERVAL:\t\t" + interval);
+            console.log("UNTIL:\t\t\t" + until);
+            console.log("COUNT:\t\t\t" + count);
+            console.log("BYDAY:\t\t\t " + byDay);
+        }
+
+        function recEvMonthly(e, byMonthDay, bySetPos, byDay, interval, until, count) {
+            console.log("\nMONTHLY");
+            console.log("NAME:\t\t\t" + e.name);
+            console.log("LOCATION:\t\t" + e.loc);
+            console.log("DATE:\t\t\t" + e.date);
+            console.log("INTERVAL:\t\t" + interval);
+            console.log("UNTIL:\t\t\t" + until);
+            console.log("COUNT:\t\t\t" + count);
+            console.log("BYMONTHDAY:\t\t" + byMonthDay);
+            console.log("BYSETPOS:\t\t" + bySetPos);
+            console.log("BYDAY:\t\t\t" + byDay);
+        }
+
+        function recEvYearly(e, byDay, bySetPos, byMonth, byMonthDay, until, count) {
+            console.log("\nYEARLY");
+            console.log("NAME:\t\t\t" + e.name);
+            console.log("LOCATION:\t\t" + e.loc);
+            console.log("DATE:\t\t\t" + e.date);
+            console.log("UNTIL:\t\t\t" + until);
+            console.log("COUNT:\t\t\t" + count);
+            console.log("BYMONTHDAY:\t\t" + byMonthDay);
+            console.log("BYSETPOS:\t\t" + bySetPos);
+            console.log("BYDAY:\t\t\t" + byDay);
+            console.log("BYMONTH:\t\t" + byMonth);
         }
 
         function drawableEventsCompare(a, b) {
-            if (a.date < b.date)
-                return -1;
+            // if (a.date < b.date)
+            //     return -1;
+            // if (a.date > b.date)
+            //     return 1;
+            // return 0;
             if (a.date > b.date)
+                return -1;
+            if (a.date < b.date)
                 return 1;
             return 0;
         }
@@ -260,35 +521,35 @@ const CALENDAR = (function () {
             while (true) {
                 times.push(drawableEvents[i].date.toTimeString());
                 names.push(drawableEvents[i].name);
-                if(i + 1 < drawableEvents.length && drawableEvents[i + 1].date.getDate() == currDate)
+                if (i + 1 < drawableEvents.length && drawableEvents[i + 1].date.getDate() == currDate)
                     i++;
                 else break;
             }
-            while(eventWrapper.children[1].children[1] != null) {
+            while (eventWrapper.children[1].children[1] != null) {
                 eventWrapper.children[1].children[1].remove();
             }
-            for(let j = 0; j < times.length; j++) {
+            for (let j = 0; j < times.length; j++) {
                 let timeWrapper = eventWrapper.children[1].children[0].cloneNode(true);
                 timeWrapper.classList.remove('display-none');
-                if(parseInt(times[j].substring(0,2)) > 12) {
-                    timeWrapper.innerHTML = parseInt(times[j].substring(0,2)) % 12 + times[j].substring(2, 5) + ' PM';
+                if (parseInt(times[j].substring(0, 2)) > 12) {
+                    timeWrapper.innerHTML = parseInt(times[j].substring(0, 2)) % 12 + times[j].substring(2, 5) + ' PM';
                 }
-                else timeWrapper.innerHTML = times[j].substring(0, 5);
-                if(times[j].substring(0,5) == '00:00')
+                else timeWrapper.innerHTML = timeWrapper.innerHTML = parseInt(times[j].substring(0, 2)) + times[j].substring(2, 5) + ' AM';
+                if (times[j].substring(0, 5) == '00:00')
                     timeWrapper.innerHTML = "All Day";
                 eventWrapper.children[1].appendChild(timeWrapper);
             }
-            while(eventWrapper.children[2].children[1] != null) {
+            while (eventWrapper.children[2].children[1] != null) {
                 eventWrapper.children[2].children[1].remove();
             }
-            for(let j = 0; j < names.length; j++) {
+            for (let j = 0; j < names.length; j++) {
                 let namesWrapper = eventWrapper.children[2].children[0].cloneNode(true);
                 namesWrapper.classList.remove('display-none');
                 namesWrapper.innerHTML = names[j];
                 eventWrapper.children[2].appendChild(namesWrapper);
             }
             eventContainer.appendChild(eventWrapper);
-            if(eventContainer.children.length > 5)
+            if (eventContainer.children.length > 10)
                 return;
         }
     };
@@ -298,7 +559,6 @@ const CALENDAR = (function () {
         _parseFile();
         _sortEvents();
         _drawCalendar();
-        console.log("UPDATED");
     };
 
     const updateCalendarContinuously = () => {
